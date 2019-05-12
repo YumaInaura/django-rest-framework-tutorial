e137913782cc0fda89f4160cfb4a2fdba436e408
YumaInaura <yuma.inaura@gmail.com>
2019-05-12 11:18:19 +0900

    no named commit

---
+++ codes/create.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = Snippet(code='print("hello, world")\n')
+snippet.save()
+
+print(snippet)
+
