de2698a9488fdca0ec038596dfaf64472d97c1a0
YumaInaura <yuma.inaura@gmail.com>
2019-05-12 11:35:37 +0900

    no named commit

--- codes/create.py
+++ codes/create.py
@@ -5,10 +5,12 @@ from snippets.serializers import SnippetSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
+import json
+
 snippet = Snippet(code='print("hello, world")\n')
 snippet.save()
 
 serializer = SnippetSerializer(snippet)
 
-print(serializer.data)
+print(json.dumps(serializer.data))
 
